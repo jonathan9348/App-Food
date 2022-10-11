28eda34919b9b373bca704d16c4e4a65aca5d00c
unknown <jonathan.moran9348@gmail.com>
2022-10-11 02:13:09 -0300

    Empezando el Front

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -34,6 +34,12 @@ module.exports = (sequelize) => {
       image: {
         type: DataTypes.STRING,
       },
+
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      }
     },
     { timestamps: false }
   );
