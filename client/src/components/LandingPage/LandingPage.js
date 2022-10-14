30c76da64601ff02885ac0f24c7f091866762b1e
unknown <jonathan.moran9348@gmail.com>
2022-10-14 02:38:23 -0300

    Estilos

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import landingPage from '/Users/usuario/Desktop/App-Food/client/src/assets/landingPage.jpg';
+import '../LandingPage/LandingPage.css'
 
 export default function LandingPage() {
   return (
     <div>
-      <div>
+      <div className="cont-txt">
         <h1>Food's</h1>
       </div>
 
-      <img src="" alt="" className="" />
+      <img src={landingPage} alt="img not found" className="body-img" />
 
       <Link to="/home">
-        <button type="submit" className="">
+        <button type="submit" className="bnt-home">
           The food is served!
         </button>
       </Link>
